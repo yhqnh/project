fe575d55d91f6e289f1e2a783961dac45268ba1e
yhqnh <yhqnh@aliyun.com>
2016-01-08 15:39:17 +0800

    init

--- project-dal/src/main/java/com/project/dao/impl/AbstractSessionFactory.java
+++ project-dal/src/main/java/com/project/dao/impl/AbstractSessionFactory.java
@@ -3,11 +3,13 @@ package com.project.dao.impl;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.support.SqlSessionDaoSupport;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public abstract class AbstractSessionFactory extends SqlSessionDaoSupport{
 	
 	@Autowired
+	@Override
 	public void setSqlSessionFactory(SqlSessionFactory sqlSessionFactory) {
 		super.setSqlSessionFactory(sqlSessionFactory);
 	}
